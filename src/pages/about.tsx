43a6d6b1a07f223a3b3219c641a651cbe9d03382
Kat Keilty <85399179+KatKeilty@users.noreply.github.com>
2026-08-09 17:21:38 +0000

    started the about page

---
+++ src/pages/about.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+const MISSION = {
+        eyebrow: 'My Mission'
+        headline: 'Learning should be fun, and shared!'
+        body: [
+            'As a professional I spend a lot of time creating content, designing, developing and delivering training.',
+            'Most of that training is delivered on software topics, but also process improvement and general training initiatives.',
+            'I learned much of what I know through online tutorials, hit-and-miss experimentation, and now more often thn not, with the aid of LLMs.',
+            'In fact, this site is a learning project of my own. I am learning more about react, modeling this site off of a figma make project developed by prompts.',
+            'To create your own library, use this published template on figma make!, Be sure to let me know and I will link to it here.'
+        ]
+}
